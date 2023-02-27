99b186f56fb182a011c23891a415f0ad85afe317
ylee111 <43230353+yunseonlee@users.noreply.github.com>
2023-02-26 23:35:32 -0500

    validation works

--- partb/calculator.js
+++ partb/calculator.js
@@ -4,11 +4,11 @@ function handleSubmit(event)
 {
     let mortgageAmount = document.getElementById("mortgageAmount");
     let interestRate = document.getElementById("interestRate");
-
     let loanLength = document.getElementById("loanLength");
-    let postalCode = document.getElementById("postalCode");
+    let postalCode = document.getElementById("postalCode").value;
 
-
+    let errors=[];
+ 
      output.innerHTML = "mortgageAmount: " + mortgageAmount.value + 
     "<br />interestRate: " + interestRate.value;
     "<br />loanLength: " + loanLength.value
@@ -17,22 +17,46 @@ function handleSubmit(event)
      let new_mortgageAmount = parseInt(mortgageAmount.value);
      let new_interestRate = parseInt(interestRate.value);
      let new_loanLength = parseInt(loanLength.value);
-
- 
+  
     if (Number.isInteger(new_mortgageAmount))
     {
-        output.innerHTML = "Mortgage Amount must be a positive number."
+        errors.push("Mortgage Amount must be a positive number.");
     }
 
     if (Number.isInteger(new_interestRate))
     {
-        output.innerHTML = "Interest rate Amount must be a positive number."
+        errors.push("Interest rate Amount must be a positive number.");
+        // output.innerHTML = "Interest rate Amount must be a positive number."
     }
+
     if ( new_loanLength>5 && new_loanLength<30)
     {
-        output.innerHTML = "Valid Loan length"
+        errors.push("Valid Loan length")
     }else{
-        output.innerHTML = "Loan length must be between 5-30 years."
+        errors.push("Loan length must be between 5-30 years.");
+    }
+
+    console.log(postalCode.length);
+    if(postalCode.length>6 && postalCode.length<8 && postalCode[0]=="L")
+    {
+        console.log("L and 7 character working");
+    }else{
+        errors.push("Must be located in Hamilton.");
+
+    }
+    if(errors.length>0)
+    {
+          let error_list ="<ul>";
+ 
+         for (let i=0;i<errors.length; i++)
+         {
+            error_list += "<li>" + errors[i] + "</li>";
+         }
+            error_list += "</ul>";
+
+            output.innerHTML = error_list;
+            console.log(error_list);
+
     }
     event.preventDefault();
 }
