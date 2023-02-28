bd8aec1e3aeb81f41504a7e502d03d87089d66d5
ylee111 <43230353+yunseonlee@users.noreply.github.com>
2023-02-28 00:02:31 -0500

    validation is working

--- partb/calculator.js
+++ partb/calculator.js
@@ -1,7 +1,10 @@
 let calculator = document.getElementById("calculator");
 
+
 function handleSubmit(event)
-{
+{'use strict'
+handleReset();
+    var forms=document.querySelectorAll('.needs-validation')
     let mortgageAmount = document.getElementById("mortgageAmount");
     let interestRate = document.getElementById("interestRate");
     let loanLength = document.getElementById("loanLength");
@@ -9,31 +12,45 @@ function handleSubmit(event)
 
     let errors=[];
  
-     output.innerHTML = "mortgageAmount: " + mortgageAmount.value + 
-    "<br />interestRate: " + interestRate.value;
-    "<br />loanLength: " + loanLength.value
-    "<br />postalCode: " + postalCode.value;
+ 
 
      let new_mortgageAmount = parseInt(mortgageAmount.value);
      let new_interestRate = parseInt(interestRate.value);
      let new_loanLength = parseInt(loanLength.value);
+console.log(postalCode.value);
+ 
   
     if (Number.isInteger(new_mortgageAmount))
     {
+        console.log("mortgage amount is positive");
+ 
+
+    }else{
         errors.push("Mortgage Amount must be a positive number.");
+ 
+
+
     }
 
     if (Number.isInteger(new_interestRate))
     {
-        errors.push("Interest rate Amount must be a positive number.");
-        // output.innerHTML = "Interest rate Amount must be a positive number."
+        console.log("Interest amount is positive");
+       // Element.interestRate.add("is-valid");
+    }else{
+        errors.push("Interest Amount must be a positive number.");
+       // Element.interestRate.add("is-invalid");
+
     }
 
     if ( new_loanLength>5 && new_loanLength<30)
     {
-        errors.push("Valid Loan length")
+       console.log("Valid Loan length");
+      // Element.loanLength.add("is-valid");
+
     }else{
         errors.push("Loan length must be between 5-30 years.");
+     //   Element.loanLength.add("is-invalid");
+
     }
 
     console.log(postalCode.length);
@@ -55,13 +72,41 @@ function handleSubmit(event)
             error_list += "</ul>";
 
             output.innerHTML = error_list;
-            console.log(error_list);
+ 
+    }else{
+        handleReset();
+    }
+    console.log(new_mortgageAmount);
+    console.log(new_interestRate);
+    console.log(new_loanLength);
+    console.log(postalCode);
+ 
+
+
+ 
+    event.preventDefault();
+
+}
+function handleReset(event){
+    console.log("reset");
+     output.innerHTML = "";
+  
+    new_mortgageAmount=0;
+    new_interestRate=0;
+    new_loanLength=0;
+     error_list="";
+    errors=[];
+    console.log(error_list);
+    console.log(new_mortgageAmount);
+    console.log(new_interestRate);
+    console.log(new_loanLength);
+    console.log(postalCode);
+
+
 
     }
-    event.preventDefault();
-}
-
-calculator.addEventListener("submit",handleSubmit);
+calculator.addEventListener("click",handleSubmit);
+reset.addEventListener("click",handleReset);
 
 
  
